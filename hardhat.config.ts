8ece70352e55f632c1b1609503ce8fef89e2868b
Joao Paulo Furtado <joaopaulofurtado@live.com>
2022-11-03 19:36:35 -0700

    solidity bump

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -25,7 +25,7 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 // Go to https://hardhat.org/config/ to learn more
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.0",
+  solidity: "0.8.1",
   networks: {
     // comment this in if you want to use ganache UI. Currently it's using the ganache-cli automatically!
     localhost: {
